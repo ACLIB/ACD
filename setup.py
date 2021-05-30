626a0f71fb3134aa3ad383ed5977415033d5e56e
styinx <zornchrisjo@gmail.com>
2021-05-30 22:10:32 +0200

    Update setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
 from distutils.core import setup, Extension
 
+
+class Library:
+    def __init__(self, name: str, sources: list, include_dirs: list):
+        self._name = name
+        self._sources = sources
+        self._include_dirs = include_dirs
+
+    def __call__(self, *args, **kwargs):
+        return [self._name, {'sources': self._sources, 'include_dirs': self._include_dirs}]
+
+
+common_lib = Library(
+    name='ACLIBCommon',
+    sources=[
+        '../Common/source/util/Mutex.cpp',
+        '../Common/source/util/LockGuard.cpp',
+        '../Common/source/util/Thread.cpp'],
+    include_dirs=['../Common/include'])
+
+
 extension = Extension(
     name='aclib_converter',
     sources=[
         'source/python/ACLIBConverter.cpp',
         'source/Converter.cpp'
     ],
-    include_dirs=['source'],
-    extra_compile_args=["/EHsc"], )
+    include_dirs=['source', '../Common/include'],
+    library_dirs=[],
+    libraries=['ACLIBCommon'],
+    extra_compile_args=['/Ot'])
+
 
 setup(name='aclib_converter',
       version='1.0',
-      ext_modules=[extension])
+      ext_modules=[extension],
+      libraries=[common_lib()])
